0ad039d2aaec22c7473679145fba2d03a5219e4c
Harikrishnan.U <lawliet.oradoraudaz@gmail.com>
2025-09-27 06:27:18 +0530

    Create DSA-4.py

---
+++ DSA-4.py
@@ -0,0 +1,18 @@
+def min(a):
+    mini=float("inf")
+    for num in a:
+        if num<mini:
+            mini=num
+    return mini
+def max(a):
+    maxi=float("inf")
+    for num in a:
+        if num>maxi:
+            maxi=num
+    return maxi
+n=int(input("Enter the length of array: "))
+nrr=[]
+for x in range(n):
+    nrr[x]=int(input("Enter the elements: "))
+    
+print(f"The min and max of the array{nrr} is {min(nrr)} and {max(nrr)}")
